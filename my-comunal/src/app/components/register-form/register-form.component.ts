5dbda5e780fe26e6e66d0418eddf16d099693274
maliciacanv <cantenovictoriomaruja@gmail.com>
2019-05-14 14:18:54 -0500

    agregando imput para la ft

--- my-comunal/src/app/components/register-form/register-form.component.ts
+++ my-comunal/src/app/components/register-form/register-form.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
   styleUrls: ['./register-form.component.css']
 })
-export class RegisterFormComponent implements OnInit {
+export class RegisterFormComponent {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+
 
 }
